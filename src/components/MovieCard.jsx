8b2fb7db0e64fe312bed33948dc85808b8507408
K4350 <kaushik4350@gmail.com>
2025-11-22 00:46:11 +0530

    watchlist UI toggle

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "../styles.css";
+import Watchlist from "./Watchlist";
 
-export default function MovieCard({ movie, isWatchlisted , toggleWatchlist }) {
+export default function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
   const handleError = (e) => {
     e.target.src = "images/default.jpg";
   };
@@ -22,10 +23,25 @@ export default function MovieCard({ movie, isWatchlisted , toggleWatchlist }) {
       />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
-        <p className="movie-card-genre">{movie.genre}</p>
-        <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
-          {movie.rating}
-        </p>
+        <div>
+          <span className="movie-card-genre">{movie.genre}</span>
+          <span className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+            {movie.rating}
+          </span>
+        </div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isWatchlisted}
+            onChange={() => toggleWatchlist(movie.id)}
+          ></input>
+
+          <span className="slider">
+            <span className="slider-label">
+              {isWatchlisted ? "In Watchlist" : "Add to Watchlist"}
+            </span>
+          </span>
+        </label>
       </div>
     </div>
   );
