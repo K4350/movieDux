7b70c64571d4a5c4da555488f447ac2495e08db3
K4350 <kaushik4350@gmail.com>
2025-11-23 22:25:22 +0530

    fixed the logo not loaded issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,11 @@ import "../styles.css";
 export default function Header() {
   return (
     <div className="header">
-      <img className="logo" src="logo.png" alt="moviedux" />
-      <h2 className="app-subtitle"> It's time for popcorn! Find your next movie here. </h2>
+      <img className="logo" src="/logo.png" alt="moviedux" />
+      <h2 className="app-subtitle">
+        {" "}
+        It's time for popcorn! Find your next movie here.{" "}
+      </h2>
     </div>
   );
 }
